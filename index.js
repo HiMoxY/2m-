f0de5af4f890dd272f7574071006e95e41254ddc
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-03-31 19:33:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ client.on('message', message => {
     if (message.member.voiceChannel) {       
         message.member.voiceChannel.join()
         .then(connection => {
-        connection.playStream("http://radio2m.scdn.arkena.com/live.mp3"); //for playing an audiostream/radio
+        connection.playStream("http://20043.live.streamtheworld.com/SKYRADIO.mp3"); //for playing an audiostream/radio
         })
         .catch(console.log);
         message.reply('Joined and started playing.');
